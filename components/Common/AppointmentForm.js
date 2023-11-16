afa35d7ddfb8985f330cc525218193dd68f3c13d
Arpit Ujjwal <arpitujjwal@192.168.1.10>
2023-11-16 21:49:50 +0530

    form-again

--- components/Common/AppointmentForm.js
+++ components/Common/AppointmentForm.js
@@ -10,96 +10,80 @@ const AppointmentForm = () => {
             <span>We will confirm your appointment within 2 hours</span>
 
             <div className="appointment-form">
-              <form>
-                <div className="row">
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <i className="icofont-business-man-alt-1"></i>
-                      <label>Name</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter Your Name"
-                      />
-                    </div>
-                  </div>
+                  <form>
+                    <div className="row">
+                      <div className="col-lg-6">
+                        <div className="form-group">
+                          <i className="icofont-business-man-alt-1"></i>
+                          <label>Name</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter Your Name"
+                          />
+                        </div>
+                      </div>
 
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <i className="icofont-ui-message"></i>
-                      <label>Email</label>
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Enter Your Email"
-                      />
-                    </div>
-                  </div>
+                      <div className="col-lg-6">
+                        <div className="form-group">
+                          <i className="icofont-ui-call"></i>
+                          <label>Phone</label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter Your Number"
+                          />
+                        </div>
+                      </div>
 
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <i className="icofont-ui-call"></i>
-                      <label>Phone</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Enter Your Number"
-                      />
-                    </div>
-                  </div>
+                      <div className="col-lg-6">
+                        <div className="form-group">
+                          <i className="icofont-hospital"></i>
+                          <label>Services</label>
+                          <select
+                            className="form-control"
+                            id="exampleFormControlSelect1"
+                          >
+                            <option>Hair</option>
+                            <option>Skin</option>
+                            <option>Laser</option>
+                            <option>Others</option>
+                          </select>
+                        </div>
+                      </div>
 
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <i className="icofont-hospital"></i>
-                      <label>Services</label>
-                      <select
-                        className="form-control"
-                        id="exampleFormControlSelect1"
-                      >
-                        <option>Dental Care</option>
-                        <option>Neurology</option>
-                        <option>Cardiology</option>
-                        <option>Pathology</option>
-                      </select>
-                    </div>
-                  </div>
+                      <div className="col-lg-6">
+                        <div className="form-group">
+                          <i className="icofont-doctor"></i>
+                          <label>Timing</label>
+                          <select
+                            className="form-control"
+                            id="exampleFormControlSelect2"
+                          >
+                            <option>10:00 AM</option>
+                            <option>11:00 AM</option>
+                            <option>12:00 PM</option>
+                            <option>01:00 PM</option>
+                            <option>02:00 PM</option>
+                            <option>03:00 PM</option>
+                            <option>04:00 PM</option>
+                            <option>05:00 PM</option>
+                            <option>06:00 PM</option>
+                            <option>07:00 PM</option>
+                            <option>08:00 PM</option>
+                          </select>
+                        </div>
+                      </div>
 
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <i className="icofont-doctor"></i>
-                      <label>Doctor</label>
-                      <select
-                        className="form-control"
-                        id="exampleFormControlSelect2"
-                      >
-                        <option>Choose Your Doctor</option>
-                        <option>John Smith</option>
-                        <option>Sarah Taylor</option>
-                        <option>Stevn King</option>
-                      </select>
                     </div>
-                  </div>
 
-                  <div className="col-lg-6">
-                    <div className="form-group">
-                      <i className="icofont-business-man"></i>
-                      <label>Age</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Your Age"
-                      />
+                    <div className="text-center">
+                      <button type="submit" className="btn appointment-btn">
+                        Submit
+                      </button>
                     </div>
-                  </div>
+                  </form>
                 </div>
-
-                <div className="text-center">
-                  <button type="submit" className="btn appointment-btn">
-                    Submit
-                  </button>
-                </div>
-              </form>
-            </div>
           </div>
         </div>
       </div>
